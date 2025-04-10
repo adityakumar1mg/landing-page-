8f07e7a87042b91c3654b8754973d6ac32a408f9
adityakumar1mg <aditya.kumar4@1mg.com>
2025-04-10 05:21:59 +0000

    env

---
+++ 1mg/src/component/card/smallCard.js
@@ -0,0 +1,18 @@
+ 
+function smallCard(){
+    return(
+        <>
+        <div className="border bg-red-400 flex flex-col text-center	">
+            <h2 className="text-2xl font-bold">Order medicine </h2>
+            <div className="">
+                <p>Medicines</p>
+            </div>
+            <div>
+                <img></img>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default  smallCard;
